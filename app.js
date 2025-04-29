fe0d7618e92ac4e93c5a48c4170009696af9497f
Muhammad Sinan <ms4730435@gmail.com>
2025-04-29 10:49:44 +0530

    registeration api

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const pool = require("./config/db");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 app.use(express.json());
@@ -43,6 +45,35 @@ app.post("/products", async (req, res) => {
   }
 });
 
+app.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const userExist = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+    if (userExist.rows.length > 0) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = await pool.query(
+      "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *",
+      [name, email, hashedPassword]
+    );
+    const token = jwt.sign(
+      { id: newUser.rows[0].id },
+      "process.env.JWT_SECRET",
+      { expiresIn: "1h" }
+    );
+    res
+      .status(201)
+      .json({ message: "User registered successfully", token: token });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
 app.listen(9000, () => {
   console.log(`Server running on port`);
 });
